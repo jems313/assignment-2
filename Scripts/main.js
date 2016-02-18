996696a524c704301d50a7023749f7b0f778153a
Jaymin Patel <jayminpatel_2@yahoo.com>
2016-02-18 13:54:24 -0500

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,42 +1,63 @@
 "use strict";
+
+(function () {
+
+    if (document.getElementById("aboutme") != null) {
+        console.log("About Page");
+    } else if (document.getElementById("project") != null) {
+        console.log("Project Page");
+    } else if (document.getElementById("contactme") != null) {
+        console.log("Contact Page");
+        //declare variables and create a reference to form elements
+        var name = document.getElementById("name");
+        var email = document.getElementById("email");
+        var mobile = document.getElementById("mobile");
+        var message = document.getElementById("message");
+        var submitButton = document.getElementById("submit");
+        submitButton.pre
+
+
+        submitButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            console.log("Name: " + name.value);
+            console.log("Email: " + email.value);
+            console.log("Contact No: " + mobile.value);
+            console.log("Query: " + message.value);
+        });
+
+
+    } // end else if
+
+    
+})();
+
 function replaceMyInfo() {
-    var info;
-    info = document.getElementById("myself");
-    info.innerHTML = "I am JaYmIn PaTeL, an International Student came from India, my hobbies are watch T-20 Cricket Match, Surffing Internat, in india  i completed M.C.A(Master of Computer Application)";
+    var myself;
+    myself = document.getElementById("myself");
+    myself.innerHTML = "I am JaYmIn PaTeL, an International Student came from India, my hobbies are watch T-20 Cricket Match, Surffing Internat, in india  i completed M.C.A(Master of Computer Application)";
 };
-replaceMyInfo();
+replaceMyInfo()
 
-var button=document.getElementById("submit");
-
-button.addEventListener("click",function() {
-        console.log("clicked...");
-});
-
-function validateForm() 
-			{
+function validateForm() {
 				var a = document.forms["contact"]["name"].value;
 				var b = document.forms["contact"]["email"].value;
 				var c = document.forms["contact"]["mobile"].value;
 				var d = document.forms["contact"]["message"].value;
 
-				if (a == null || a == "")
-				{
-					alert("Name must be filled out");
-					return false;
+				if (a == null || a == "") {
+        alert("Name must be filled out");
+        return false;
 				}
-				else if(b == null || b == "")
-				{
-					alert("Email Address must be filled out");
-					return false;
+				else if (b == null || b == "") {
+        alert("Email Address must be filled out");
+        return false;
 				}
-				else if(c == null || c == "")
-				{
-					alert("Contact Number must be filled out");
-					return false;
+				else if (c == null || c == "") {
+        alert("Contact Number must be filled out");
+        return false;
 				}
-				else if(d == null || d == "")
-				{
-					alert("Enter your Message, Query, Comments ");
-					return false;
+				else if (d == null || d == "") {
+        alert("Enter your Message, Query, Comments ");
+        return false;
 				}
-			}
+}
